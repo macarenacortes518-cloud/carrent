45c371dec9179894052082641285b1b6bebb07ce
macarenacortes518-cloud <macarenacortes518@gmail.com>
2025-12-03 16:38:13 -0300

    Main.java

--- src/carrent/Main.java
+++ src/carrent/Main.java
@@ -1,7 +1,13 @@
 package carrent;
 
+import javax.swing.SwingUtilities;
+
 public class Main {
 	public static void main (String [] args) {
-		
+		GestionCarRent sistema = new GestionCarRent(); 
+		VistaArriendoCuotas vistaArriendoCuotas = new VistaArriendoCuotas(sistema);
+		vistaArriendoCuotas.setVisible(true);
 	}
 }
+
+
